055fca9818efbe5e5c95b5c05a0f9687edaad224
pmc-a <peter.mcaree@slicelife.com>
2019-05-04 11:59:03 +0100

    add: winning, losing and loading bottas

---
+++ components/LosingBottas/index.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+import './style.css';
+
+const LosingBottas = ({ latestRaceResults }) => (
+  <Fragment>
+    <h3>No, it looks like Valtteri didn't have his porridge before the {latestRaceResults.raceName}</h3>
+    <img className='losing-bottas' src='https://pbs.twimg.com/media/Db9sKkZWAAE-npl?format=jpg&name=medium' alt='Losing Valtteri Bottas' />
+    <p>Photo Source: <a href='https://twitter.com/valtteribottas/status/990629382589992961'>Valtteri's Twitter</a></p>
+  </Fragment>
+);
+
+export default LosingBottas;
--- index.js
+++ index.js
@@ -1,34 +1,45 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
 import { render } from 'react-dom';
 import axios from 'axios';
 
-import Bottas from './components/Bottas';
+import LoadingBottas from './components/LoadingBottas';
+import LosingBottas from './components/LosingBottas';
+import WinningBottas from './components/WinningBottas';
 import './style.css';
 
 const fetchResults = () => axios.get('https://ergast.com/api/f1/current/last/results.json');
 
 const App = () => {
+  const [latestRaceResults, setLatestRaceResults] = useState(null);
   const [hadPorridge, setHadPorridge] = useState(null);
 
   useEffect(() => {
     fetchResults()
       .then((response) => {
+        setLatestRaceResults(response.data.MRData.RaceTable.Races[0]);
+
         if (response.data.MRData.RaceTable.Races[0].Results[0].Driver.code === 'BOT') {
-          setHadPorridge(true);
+          setHadPorridge(false);
         }
       })
       .catch((error) => {
         console.log(error);
       });
-  }, [hadPorridge]);
+  }, []);
 
   return (
-    <div>
+    <div id='main-container'>
       <h1>Has Valtteri Bottas had his porridge?</h1>
       {
         hadPorridge === null ?
-        <Bottas /> :
-        <p>{hadPorridge === true ? 'Yes': 'No'}</p>
+          <LoadingBottas /> :
+          <Fragment>
+            {
+              hadPorridge === true ? 
+                <WinningBottas latestRaceResults={latestRaceResults} />  :
+                <LosingBottas latestRaceResults={latestRaceResults} />
+            }
+          </Fragment>
       }
     </div>
   );
