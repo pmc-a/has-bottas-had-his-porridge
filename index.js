e51871908ffce185213ee8e9e56a85d684f88677
pmc-a <peter.mcaree@slicelife.com>
2019-05-04 10:04:43 +0100

    refactor: clean up api call

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { render } from 'react-dom';
+const axios = require('axios');
 
-import { fetchResults } from './api.service';
 import './style.css';
+
+const fetchResults = () => axios.get('https://ergast.com/api/f1/current/last/results.json');
 
 const App = () => {
   const [hadPorridge, setHadPorridge] = useState(null);
 
   useEffect(() => {
-    const driverWinnerCode = fetchResults();
-    console.log(driverWinnerCode);
-    
-    if (driverWinnerCode === 'BOT') {
-      setHadPorridge(true);
-      console.log(driverWinnerCode);
-    }
+    fetchResults()
+      .then((response) => {
+        if (response.data.MRData.RaceTable.Races[0].Results[0].Driver.code === 'BOT') {
+          setHadPorridge(true);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, [hadPorridge]);
 
   return (
     <div>
-      <h1>Has Bottas had his porridge?</h1>
+      <h1>Has Valterri Bottas had his porridge?</h1>
       <p>{hadPorridge === true ? 'Yes': 'No'}</p>
     </div>
   );
